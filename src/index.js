d38af034ba83063d842a8f09c75d467cc72d6212
Felix <felixomosh7@gmail.com>
2024-01-01 19:33:02 +0300

    Complted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    //console.log(e)
+    e.preventDefault();
+    let todo = e.target.elements["new-task-description"].value;
+    doList(todo);
+    //form.reset();
+  });
 });
+
+function doList(todo) {
+  let list = document.createElement("li");
+  list.textContent = todo;
+  let taskList = document.getElementById("tasks");
+  taskList.appendChild(list);
+
+  console.log(todo);
+}
